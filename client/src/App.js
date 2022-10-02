6ef0128074d130018c9d3a6bcfd1c7b4af32886c
david <austinedavid96@gmail.com>
2022-10-02 19:54:01 +0100

    checking for auto refresh

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ function App() {
       <p>hello world we are testing heroku</p>
       <h5>{datas}</h5>
       <p>god is good</p>
+      <h4>lets check if it will rebuild automatically</h4>
     </div>
   );
 }
