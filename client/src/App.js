520c5b233d53a739bc2b53bd4fc1502511685392
david <austinedavid96@gmail.com>
2022-10-03 10:51:02 +0100

    checking is the url from heroku will work

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,27 @@ import './App.css';
 import react, {useEffect, useState} from 'react'
 function App() {
   const[datas, setdatas] = useState()
+  const[element, setelement] = useState()
   useEffect(()=>{
     const backend = async()=>{
-      await axios.get('http://localhost:5000/').then((res)=> setdatas(res.data))
+      await axios.get('https://flowkitez.herokuapp.com/').then((res)=> setdatas(res.data))
       
     }
     backend()
   }, [])
+  useEffect(()=>{
+    const runner = async()=>{
+      await axios.get('https://flowkitez.herokuapp.com/api').then((res)=>setelement(res.data))
+    }
+    runner()
+  })
   return (
     <div className="App">
       <p>hello world we are testing heroku</p>
       <h5>{datas}</h5>
       <p>god is good</p>
       <h4>lets check if it will rebuild automatically</h4>
+      <h3>{element}</h3>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -6,5 +6,8 @@ app.use(cors())
 app.get('/', (req, res)=>{
     res.json("trying heroku deployment now")
 })
+app.get('/api', (req, res)=>{
+    res.send("next route is working well")
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>console.log("app is running now"))
