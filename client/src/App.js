f02fa845324688f42d6e83f14f9d9bb0d9fe2af9
david <austinedavid96@gmail.com>
2022-10-09 01:53:59 +0100

    finally added the functionality of setinterval in the carousel, carousel working well now

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import axios from 'axios'
 import './App.css';
 import react, {useEffect, useState} from 'react'
 import Form from './components/Form';
+import Carousel from './components/Carousel';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
  
   return (
    <div>
-    <Form/>
+    <Router>
+    <Routes>
+      <Route path='/' element={<Form/>}/>
+      <Route path='carousel' element={<Carousel/>}/>
+      
+    </Routes>
+    </Router>
    </div>
   );
 }
