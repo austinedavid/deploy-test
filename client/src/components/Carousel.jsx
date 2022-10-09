6083b5cce1056ccd5965ed0978e6ec24cbd979fb
david <austinedavid96@gmail.com>
2022-10-09 01:53:59 +0100

    finally added the functionality of setinterval in the carousel, carousel working well now

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,120 @@
+import React,{useState, useEffect} from 'react'
+import styled from 'styled-components'
+import photo1 from '../images/acer2.png'
+import photo2 from '../images/acer3.jpg'
+import photo3 from '../images/apple1.jpg'
+import photo4 from '../images/apple5.jpg'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #9fd99f;
+`
+const Wrapper = styled.div`
+    width: 500px;
+    height: 300px;
+    background-color: black;
+    border-radius: 10px;
+    overflow: hidden;
+    position: relative;
+`
+const Ul = styled.ul`
+    list-style: none;
+    width: 500px;
+    height: 300px;
+    display: flex;
+    transform: translateX(${(prop)=> prop.page * -100}%);
+    transition: all 0.6s ease-in-out;
+   
+`
+const Li = styled.li`
+   
+`
+const Img = styled.img`
+    width: 500px;
+    height: 300px;
+    object-fit: contain;
+`
+const Next = styled.div`
+    position: absolute;
+    background-color: #9fd99f;
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    z-index: 999;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
+const Back = styled.div`
+    
+    position: absolute;
+    background-color: #9fd99f;
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    z-index: 999;
+    top: 50%;
+    left: 10px;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
+
+// our jsx appears here
+const Carousel = () => {
+    const [page, setpage] = useState(0)
+    
+    console.log(page)
+    // carousel button function controls
+    const handleBack = ()=>{
+        setpage((prev)=> prev +1)
+    }
+
+    const handleNext = ()=>{
+        setpage((prev)=> prev +1)
+    }
+    
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            if(page<3){
+
+                setpage(page+1)
+            }else{
+                setpage(0)
+            }
+        }, 4000)
+        return ()=> clearInterval(interval)
+    },[page])
+ console.log(page)
+  return (
+    <Container>
+        <Wrapper>
+            {page > 0 && <Back onClick={handleBack}><ArrowBackIosNewIcon/></Back> }
+            
+            <Ul page={page}>
+                <Li ><Img src={photo1}/></Li>
+                <Li><Img src={photo2}/></Li>
+                <Li><Img src={photo3}/></Li>
+                <Li><Img src={photo4}/></Li>
+            </Ul>
+            {page < 3 && <Next onClick={handleNext}><ArrowForwardIosIcon/></Next>}
+            
+        </Wrapper>
+        
+    </Container>
+  )
+}
+
+export default Carousel
